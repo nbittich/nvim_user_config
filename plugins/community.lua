f840a1697bd26a051512c094fef71aaaf8aa8bc2
Nordine Bittich <nordine@bittich.be>
2023-03-18 11:02:36 +0100

    small fixes

--- plugins/community.lua
+++ plugins/community.lua
@@ -1,40 +1,51 @@
 return {
-  -- Add the community repository of plugin specifications
-  "AstroNvim/astrocommunity",
-  -- example of imporing a plugin, comment out to use it or add your own
-  -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
+    -- Add the community repository of plugin specifications
+    "AstroNvim/astrocommunity",
+    -- example of imporing a plugin, comment out to use it or add your own
+    -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
-  { import = "astrocommunity.colorscheme.catppuccin" },
-    { -- further customize the options set by the community
-    "catppuccin",
-    opts = {
-      flavour = "mocha",
-      term_colors = true,
-      transparent_background = true,
-      no_italic = false,
-      no_bold = false,
-      styles = {
-          comments = {},
-          conditionals = {},
-          loops = {},
-          functions = {},
-          keywords = {},
-          strings = {},
-          variables = {},
-          numbers = {},
-          booleans = {},
-          properties = {},
-          types = {},
-      },
-      color_overrides = {
-          mocha = {
-              base = "#000000",
-              mantle = "#000000",
-              crust = "#000000",
-          },
-      },
-    
+    { import = "astrocommunity.colorscheme.catppuccin" },
+    {
+        -- further customize the options set by the community
+        "catppuccin",
+        opts = {
+            flavour = "mocha",
+            term_colors = true,
+            transparent_background = true,
+            no_italic = false,
+            no_bold = false,
+            styles = {
+                comments = {},
+                conditionals = {},
+                loops = {},
+                functions = {},
+                keywords = {},
+                strings = {},
+                variables = {},
+                numbers = {},
+                booleans = {},
+                properties = {},
+                types = {},
+            },
+            highlight_overrides = {
+                mocha = function(C)
+                    return {
+                        TabLineSel = { bg = C.pink },
+                        NotifyBackground = { bg = C.pink },
+                        CmpBorder = { fg = C.surface2 },
+                        Pmenu = { bg = C.none },
+                        TelescopeBorder = { link = "FloatBorder" },
+                    }
+                end,
+            },
+            color_overrides = {
+                mocha = {
+                    base = "#000000",
+                    mantle = "#000000",
+                    crust = "#000000",
+                },
+            },
+        },
     },
-  },
-  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
+    -- { import = "astrocommunity.completion.copilot-lua-cmp" },
 }
