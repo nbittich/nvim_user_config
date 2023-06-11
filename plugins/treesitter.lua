9f8507fd7cb4a065bf67b522ac95e9aa02419593
Nordine Bittich <nordine@bittich.be>
2023-06-11 10:28:37 +0200

    add plugin

--- plugins/treesitter.lua
+++ plugins/treesitter.lua
@@ -1,6 +1,13 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  opts = {
-    ensure_installed = { "lua", "java", "rust", "typescript" },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = { "lua", "java", "rust", "typescript" },
+    }
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    after = "nvim-treesitter",
+    requires = "nvim-treesitter/nvim-treesitter",
+  }
 }
